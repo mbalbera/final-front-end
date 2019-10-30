1fa518935fb5c3eb755b5dc1d397c48779b64b62
Mason Balbera <mhbalbera@gmail.com>
2019-10-30 19:48:27 -0400

    signup screen and redux auth pattern

---
+++ reducers/AuthReducer.js
@@ -0,0 +1,41 @@
+import {
+    USER_SIGN_UP,
+    USER_LOGIN,
+    FETCH_USER
+} from "../Constants/actionCreators";
+import { AsyncStorage } from "react-native";
+
+const INITIAL_STATE = {
+    id: null,
+    token: {},
+    user: {},
+    userObj: {}
+};
+
+function authReducer(state = INITIAL_STATE, action) {
+    switch (action.type) {
+        case USER_SIGN_UP:
+            return {
+                ...state,
+                id: action.payload.user.id,
+                token: action.payload.user.token
+            };
+        case USER_LOGIN:
+            console.log("SET STATE");
+            return {
+                ...state,
+                id: action.payload.id,
+                token: action.payload.token,
+                user: action.payload
+            };
+        case FETCH_USER:
+            return {
+                ...state,
+                userObj: action.payload
+            };
+        default:
+            return state;
+    }
+}
+
+export { authReducer };
