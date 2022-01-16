06091fd1705630a4ee69b9e5b779624e01d4a0c2
Mason Balbera <mhbalbera@gmail.com>
2022-01-16 11:36:52 -0500

    set up recoil

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
 import React from "react";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import { rootReducer } from './reducers/index'
-import { Provider } from "react-redux";
 import AppNavigator from './navigation/AppNavigator';
+import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 console.disableYellowBox = true
-const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, storeEnhancers(applyMiddleware(thunk)));
 
 class App extends React.Component {
-  //   state={
-  //     sport: ""
-  //   }
+
   render(){
     return(
-
-      <Provider store={store}>
-          <AppNavigator />
-      </Provider>
+        <RecoilRoot>
+          <AppNavigator/>
+        </RecoilRoot>
     )}
 }
 
